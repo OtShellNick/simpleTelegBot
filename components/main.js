3c7c0dbed665b6e642be610ddafb3773956404bf
Aleksandr <exte92@gmail.com>
2022-03-18 23:20:41 +0300

    up parser

--- components/main.js
+++ components/main.js
@@ -1,7 +1,12 @@
 const { Markup} = require('telegraf');
+const {checkItems} = require("./upgradeIrem");
 
 module.exports = {
     main(ctx) {
+        setInterval(() => {
+            checkItems(ctx);
+        }, 60000);
+
         return ctx.reply('Welcome', Markup
             .keyboard([
                 ['КХ викторина']
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const {Telegraf} = require('telegraf');
 const { help } = require('./components/help');
 const { kh } = require('./components/kh');
 const { main } = require('./components/main');
 
-const bot = new Telegraf(process.env.TOKEN)
+const bot = new Telegraf(process.env.TOKEN);
 console.log('Started successfully');
-bot.start(main)
-bot.help(help)
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('КХ викторина', ctx => kh(ctx, bot))
-bot.launch(main)
+bot.start(main);
+bot.help(help);
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('КХ викторина', ctx => kh(ctx, bot));
+bot.launch(main);
